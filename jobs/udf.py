76ba248161351236d6222cc8142cde63b399503f
caoyujie <caoyujiedartz@163.com>
2024-09-12 14:49:20 +0000

    增加注释

--- jobs/udf.py
+++ jobs/udf.py
@@ -1,7 +1,15 @@
 from pyspark.sql.functions import udf
-from pyspark.sql.types import IntegerType, FloatType
+from pyspark.sql.types import FloatType
 
-# Define a UDF that squares a number
-@udf(returnType=FloatType())  # Use IntegerType for integer results
+
+@udf(returnType=FloatType())
 def square_number(s):
+    """Define a spark UDF that squares a number
+
+    Args:
+        s (double): 数字
+
+    Returns:
+        double: 平方数
+    """
     return s * s
